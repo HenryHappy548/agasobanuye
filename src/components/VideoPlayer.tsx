7e22374b0dc6fdc29e100ebcc14f79ee93fb44ed
Mr.Henry <muhirwahenryhappy@gmail.com>
2025-09-29 09:32:22 +0200

    Update VideoPlayer.tsx

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -39,9 +39,7 @@ const VideoPlayer = ({ isOpen, onClose, videoId }: VideoPlayerProps) => {
           { quality: "1080p", size: "1.2GB", url: "https://download.example.com/freakier-friday-1080p.mp4", type: "MP4" }
        
         ]
-      },
-        
-      "mana": {
+      },"mana": {
         title: "A working man A (2025)",
         embedCode: '<iframe width="1024" height="640" frameborder="0" src="https://mega.nz/embed/HsBnBKBK#ukkT0LJ4fGrdghjR9bwRrmRJYTi5cvYiN93wq5bz0fk" allowfullscreen ></iframe>',
         host: "Gaheza",
