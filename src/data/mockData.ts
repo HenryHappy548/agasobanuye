2dc18ce1795a4668d65adc6aed55e78e07ccd83b
Mr.Henry <muhirwahenryhappy@gmail.com>
2025-10-03 11:00:16 +0200

    Update mockData.ts

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -28,6 +28,7 @@ import ski from "@/assets/skin.jpeg";
 import boy from "@/assets/boy.jpeg";
 import over from "@/assets/over.jpeg";
 import cov from "@/assets/cov.jpeg";
+import mis from "@/assets/mission 1.jpeg";
 export interface Movie {
   id: string;
   title: string;
@@ -39,6 +40,25 @@ export interface Movie {
 }
 
 export const mockMovies: Movie[] = [
+  {
+    id: "misb",
+    title: "Mission impossible Dead reckoning B",
+    poster: mis,
+    year: "2023",
+    genre: "Action Thriller",
+    rating: "Rocky kimomo",
+    category: "trending"
+  },
+  {
+    id: "misa",
+    title: "Mission impossible Dead reckoning A",
+    poster: mis,
+    year: "2023",
+    genre: "Action Thriller",
+    rating: "Rocky kimomo",
+    category: "trending"
+  },
+  
   {
     id: "cov",
     title: "Coverant War",
