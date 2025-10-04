8d958eb2b009d475bc1e02cfa6e8d497a525a74f
Mr.Henry <muhirwahenryhappy@gmail.com>
2025-10-04 21:56:16 +0200

    Update mockData.ts

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -28,7 +28,7 @@ import ski from "@/assets/skin.jpeg";
 import boy from "@/assets/boy.jpeg";
 import over from "@/assets/over.jpeg";
 import cov from "@/assets/cov.jpeg";
-import mis from "@/assets/mission 1.jpeg";
+import mis from "@/assets/missiona.jpeg";
 export interface Movie {
   id: string;
   title: string;
