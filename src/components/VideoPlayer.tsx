fcdb715bd41346c2fffefaa8a84bdd918070e691
Mr.Henry <muhirwahenryhappy@gmail.com>
2025-10-07 16:12:00 +0200

    Update VideoPlayer.tsx

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -87,14 +87,7 @@ const VideoPlayer = ({ isOpen, onClose, videoId }: VideoPlayerProps) => {
         downloadLinks: [
           { quality: "1080p", size: "330MB", url: "https://streamtape.com/v/ab4jBZwm9RHxyje/S01_-_EP01_-_The_I-Land.mp4", type: "MP4" }
         ]
-        },  
-      "aar8": {
-        title: "Aar Ya Paar EP8",
-        embedCode: '<iframe src="https://streamtape.com/e/GQQAq2jx2GCaVq/" width="420" height="240" allowfullscreen allowtransparency allow="autoplay" scrolling="no" frameborder="0"></iframe>',
-        host: "Sankara",
-        downloadLinks: [
-          { quality: "1080p", size: "330MB", url: "https://streamtape.com/v/GQQAq2jx2GCaVq/S01_-_EP01_-_Aar_Ya_Paar.mp4", type: "MP4" }
-        ]
+     
         
         },  
       "aar8": {
