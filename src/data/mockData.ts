3aad7ef9df649e9bc95ce5f343f0526935968709
Mr.Henry <muhirwahenryhappy@gmail.com>
2025-10-19 10:40:12 +0200

    Fix image extension for play asset in mockData

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -42,7 +42,7 @@ import com from "@/assets/com2.jpg";
 import sec from "@/assets/375.jpeg";
 import bag from "@/assets/Baaghi 4.jpg";
 import bus from "@/assets/Bus.jpg";
-import ply from "@/assets/play.jpg";
+import ply from "@/assets/play.jpeg";
 export interface Movie {
   id: string;
   title: string;
