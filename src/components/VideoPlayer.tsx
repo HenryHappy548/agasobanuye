28de0b18afc75dcfb1a3de9c42bbfa1fc75eb30b
Mr.Henry <muhirwahenryhappy@gmail.com>
2025-11-13 21:01:31 +0200

    Update VideoPlayer.tsx

--- src/components/VideoPlayer.tsx
+++ src/components/VideoPlayer.tsx
@@ -99,6 +99,26 @@ const VideoPlayer = ({ isOpen, onClose, videoId }: VideoPlayerProps) => {
 
           ]
         },
+      "temp6": {
+        title: "Tempest S01 E6",
+        embedCode: '<iframe width="640" height="360" src="https://short.icu/1WEEfLiwG" frameborder="0" scrolling="0" allowfullscreen></iframe>',
+          host: "Junior",
+        downloadLinks: [
+          { quality: "720p", size: "780MB", url: "https://www.mediafire.com/file/1tjesx4qdcksvt4/Tempest+S01e06.mp4/file", type: "MP4" }
+          
+
+          ]
+          },
+      "temp5": {
+        title: "Tempest S01 E5",
+        embedCode: '<iframe width="640" height="360" src="https://short.icu/eRU8OcTdO" frameborder="0" scrolling="0" allowfullscreen></iframe>',
+          host: "Junior",
+        downloadLinks: [
+          { quality: "720p", size: "780MB", url: "https://www.mediafire.com/file/nefdazg48vb6t4x/Tempest+S01e05.mp4/file", type: "MP4" }
+          
+
+          ]
+        },
       "hrs2": {
         title: "24 Hours To live B",
         embedCode: '<iframe width="640" height="360" src="https://short.icu/5rBSSRKlk" frameborder="0" scrolling="0" allowfullscreen></iframe>',
