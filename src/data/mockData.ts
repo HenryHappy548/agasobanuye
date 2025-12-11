e35effbc534931cafc2033aa892b37dbaa4bf76c
Mr.Henry <muhirwahenryhappy@gmail.com>
2025-12-11 07:19:37 +0200

    Update mockData.ts

--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -109,7 +109,7 @@ import seanc from "@/assets/seamc.jpeg"
 import avenger from "@/assets/avenger.jpeg"
 import collateral from "@/assets/collateral.jpeg"
 import great from "@/assets/great.jpeg"
-import flady from "@/assets/flady.jpeg"
+import ringsl from "@/assets/ringsl.jpeg"
   export interface Movie {
   id: string;
   title: string;
@@ -121,6 +121,24 @@ import flady from "@/assets/flady.jpeg"
 }
 
 export const mockMovies: Movie[] = [
+  {
+    id: "ringslb",
+    title: "The Lord of The Ring I A",
+    poster: ringsl,
+    year: "2001",
+    genre: "Fantasy Adventure",
+    rating: "Gaheza",
+    category: "movie"
+  },
+  {
+    id: "ringsla",
+    title: "The Lord of The Ring I A",
+    poster: ringsl,
+    year: "2001",
+    genre: "Fantasy Adventure",
+    rating: "Gaheza",
+    category: "movie"
+  },
   {
     id: "flady4",
     title: "First Lady S01 E4",
