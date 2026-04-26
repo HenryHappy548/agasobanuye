0a90091cab57025d8bfcb370f5fc9dca219abd44
Henryhappy548 <muhirwahenryhappy@gmail.com>
2026-04-26 20:37:37 +0200

    Add custom Monetag vignette ad zone 10527776

--- src/components/MonetagAds.tsx
+++ src/components/MonetagAds.tsx
@@ -71,16 +71,23 @@ export const MonetagAdsBootstrap = ({
       appendMonetagScriptOnce("10527843_2", "https://nap5k.com/tag.min.js");
     }, delayMs + 3_000);
 
-    // Load third vignette banner (zone 10527712)
-    const timer3 = window.setTimeout(() => {
-      appendMonetagScriptOnce("10527712", "https://n6wxm.com/vignette.min.js");
-    }, delayMs + 6_000);
+// Load third vignette banner (zone 10527712)
+  const timer3 = window.setTimeout(() => {
+    appendMonetagScriptOnce("10527712", "https://n6wxm.com/vignette.min.js");
+  }, delayMs + 6_000);
 
-    return () => {
-      window.clearTimeout(timer1);
-      window.clearTimeout(timer2);
-      window.clearTimeout(timer3);
-    };
+  // Custom vignette ad for supporters (zone 10527776)
+  const timer4 = window.setTimeout(() => {
+    appendMonetagScriptOnce("10527776", "https://omg10.com/vignette.min.js");
+  }, delayMs + 9_000);
+
+  return () => {
+    window.clearTimeout(timer1);
+    window.clearTimeout(timer2);
+    window.clearTimeout(timer3);
+    window.clearTimeout(timer4);
+  };
+};
   }, [shouldLoad, delayMs]);
 
   return null;
